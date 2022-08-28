f0898fb9795d0d22b44a85ea7a0584010fd793b2
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-08-27 23:01:55 -0300

    Search api output JSON funcionando 5:23:28

--- services/search.js
+++ services/search.js
@@ -4,21 +4,21 @@ const APP_ID = process.env.APP_ID; // Node.js
 const API_KEY = process.env.API_KEY;
 
 const client = algoliasearch(APP_ID, API_KEY);
-const index = client.initIndex("prod_comics");
+const index = client.initIndex("xkcd");
 
-const CACHE = {};
+// const CACHE = {};
 
 export const search = async ({ query }) => {
-  if (CACHE[query]) {
-    return { results: CACHE[query] };
-  }
+  // if (CACHE[query]) {
+  //   return { results: CACHE[query] };
+  // }
 
   const { hits } = await index.search(query, {
     attributesToRetrieve: ["id", "title", "img", "alt"],
     hitsPerPage: 10,
   });
 
-  CACHE[query] = hits;
+  // CACHE[query] = hits;
 
   return { results: hits };
 };
