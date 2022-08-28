6fde732bc3e630b454a3ca7c0ea4b99dd2115ca7
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-08-28 08:16:13 -0300

    funciona buscador pero no limpia aun listado de resultados 6:10:33

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { useState, useRef } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Header() {
+  const [results, setResults] = useState([]);
+  const searchRef = useRef();
+  const { locale, locales } = useRouter();
+
+  const getValue = () => searchRef.current?.value;
+
+  const handleChange = () => {
+    const q = getValue();
+
+    if (!q) return;
+
+    fetch(`/api/search?q=${q}`)
+      .then((res) => res.json())
+      .then((searchResults) => {
+        setResults(searchResults);
+      });
+    
+  };
+
+  const handleClick = () => { 
+    searchRef.current.value = "";
+  }
+
+  // const restOfLocales = locales.filter((l) => l !== locale);
+
   return (
     <header className="flex items-center justify-between max-w-xl p-4 m-auto">
       <h1 className="font-bold">
@@ -20,9 +46,41 @@ function Header() {
             </Link>
           </li>
           <li>
-            <Link href="/">
-              <a className="text-sm font-semibold">Search</a>
-            </Link>
+            <input
+              className="px-4 py-1 text-xs border border-gray-400 rounded-3xl"
+              type="text"
+              onChange={handleChange}
+              ref={searchRef}
+            />
+            <button onClick={handleClick}> X</button>
+
+            <div className="relative z-10">
+              {Boolean(results.length) && (
+                <div className="absolute top-0 left-0">
+                  <ul className="z-50 w-full overflow-hidden bg-white border rounded-lg shadow-xl border-gray-50">
+                    <li className="m-0" key="all-results">
+                      <Link href={`/search?q=${getValue()}`}>
+                        <a className="block px-2 py-1 overflow-hidden text-sm italic font-semibold text-gray-400 hover:bg-slate-200 text-ellipsis whitespace-nowrap">
+                          Ver {results.length} resultados
+                        </a>
+                      </Link>
+                    </li>
+
+                    {results.map((result) => {
+                      return (
+                        <li className="m-0" key={result.id}>
+                          <Link href={`/comic/${result.id}`}>
+                            <a className="block px-2 py-1 overflow-hidden text-sm font-semibold hover:bg-slate-200 text-ellipsis whitespace-nowrap">
+                              {result.title}
+                            </a>
+                          </Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              )}
+            </div>
           </li>
         </ul>
       </nav>
